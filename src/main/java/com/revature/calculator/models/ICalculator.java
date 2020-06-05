2fbda60b0140a2c213ff877dcb2dae837cf36ed3
jorlando989 <jorlando989@gmail.com>
2020-06-05 19:07:39 -0400

    delete files not in folder

--- src/main/java/com/revature/calculator/models/ICalculator.java
+++
@@ -1,8 +0,0 @@
-package com.revature.calculator.models;
-
-public interface ICalculator<T> {
-	public Number add(T a, T b);
-	public Number subtract(T a, T b);
-	public Number divide(T a, T b);
-	public Number multiply(T a, T b);
-}
