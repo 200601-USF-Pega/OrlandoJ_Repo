d68288264f8453220d41524ae4de7a62e95ca40f
jorlando989 <jorlando989@gmail.com>
2020-06-17 17:12:55 -0400

    upload servlet example

---
+++ servletexample/src/main/java/com/revature/servletexample/servlets/ServletOne.java
@@ -0,0 +1,30 @@
+package com.revature.servletexample.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletOne extends HttpServlet {
+	private static final long serialVersionUID = -465940822375880622L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        System.out.println("In the get method of ServletOne");
+        
+        res.setContentType("text/html");
+        res.getWriter().write(""
+        		+ "<html> "
+        		+ "<body> "
+        		+ "<p> welcome to the get method </p>"
+        		+ "</body>"
+        		+ "</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    }
+}
